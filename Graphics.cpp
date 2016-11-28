65760e4a2b337003ba9f506773f82cb746726ee7
FiljoA <monsterjamp@yahoo.com>
2016-11-28 11:29:42 -0600

    Fix drawing position: SFML draws from bottom to top

--- Graphics.cpp
+++ Graphics.cpp
@@ -7,13 +7,13 @@ void Chip8::ClearScreen() {
 
 void Chip8::DrawPixel(uint8_t x, uint8_t y) {
 	pixel.setFillColor({255, 255, 255, 255});
-	pixel.setPosition(x * upscale, y * upscale);
+	pixel.setPosition(x * upscale - upscale, y * upscale - upscale);
 	app->draw(pixel);
 	app->display();
 }
 void Chip8::ErasePixel(uint8_t x, uint8_t y) {
 	pixel.setFillColor({0, 0, 0, 255});
-	pixel.setPosition(x * upscale, y * upscale);
+	pixel.setPosition(x * upscale - upscale, y * upscale - upscale);
 	app->draw(pixel);
 	app->display();
 }
